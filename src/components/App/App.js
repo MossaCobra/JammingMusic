5483435ecde68b612eda7542dbdff3bf12791d49
MossaCobra <jaydengalea11@gmail.com>
2024-05-30 18:19:02 +1000

    Changed files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,40 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import SearchBar from '../SearchBar/SearchBar';
-import SearchResult from '../SearchResults/SearchResult';
-import Playlist from '../Playlist/Playlist';
 
-import { Container, Row, Col } from 'react-bootstrap';
-
-function App() {
-  const [playlistName, setPlaylistName] = useState("My Playlist");
-  const [playlistTracks, setPlaylistTracks] = useState([]);
-
-  const handleRemoveTrack = (index) => {
-    const updatedTracks = [...playlistTracks];
-    updatedTracks.splice(index, 1);
-    setPlaylistTracks(updatedTracks);
-  };
-
+function App () {
+  
   return (
-    <div className="App">
-      <h1>Jammming</h1>
-      <SearchBar />
-      <Container fluid>
-        <Row>
-          <Col md={6}>
-            <SearchResult />
-          </Col>
-          <Col md={6}>
-            {/* Pass down playlistName and playlistTracks to the Playlist component */}
-            <Playlist
-              playlistName={playlistName}
-              songs={playlistTracks}
-              handleRemove={handleRemoveTrack}
-            />
-          </Col>
-        </Row>
-      </Container>
+    <div>
+        <h1>Ja<span className="highlight">mmm</span>ing Music</h1>
+        <div className="App">
+      </div>
     </div>
   );
 }
